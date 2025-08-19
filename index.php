563a7f4cecf7ac265a8f769b7ebf300785a2ac5a
Eduardo Camillo <eduardo.camillo15@gmail.com>
2025-08-18 21:29:58 -0300

    structing the project

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
 <?php
 require('config.php');
 
-echo '<h1> Fiados App! </h1>';
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./assets/style.css">
+    <title>Vendas fiado</title>
+</head>
+<body>
+    <script src="./assets/index.js"> </script>
+    <nav class="nav-bar">
+        <ul>
+            <li>
+               <a onClick="location.reload();"> Home </a>
+            </li>
+            <li>
+                <a id="clientes" href="#"> Clientes </a>
+            </li>
+            <li>
+                <a id="produtos" href="#"> Produtos </a>
+            </li>
+        </ul>
+
+        
+    </nav>
+</body>
+</html>
